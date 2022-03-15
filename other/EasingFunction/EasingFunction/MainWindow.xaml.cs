4263338027cdb5ad311e6a2137cf7787399f09b3
shaw <1939484341@qq.com>
2022-03-15 10:58:30 +0800

    缓动函数

---
+++ other/EasingFunction/EasingFunction/MainWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasingFunction
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		//动画时长
+		private int iDuration { get; set; } = 1;
+		//缓动类型
+		private LEaseMode easeMode { get; set; } = LEaseMode.EaseIn;
+		private Storyboard sBoard = null;
+		private string sType { get; set; } = "";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			this.Loaded += Window_Loaded;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				ComboBox1.Items.Add("Back");
+				ComboBox1.Items.Add("Bounce");
+				ComboBox1.Items.Add("Circle");
+				ComboBox1.Items.Add("Quadratic");
+				ComboBox1.Items.Add("Cubic");
+				ComboBox1.Items.Add("Quartic");
+				ComboBox1.Items.Add("Quintic");
+				ComboBox1.Items.Add("Elastic");
+				ComboBox1.Items.Add("Exponential");
+				ComboBox1.Items.Add("Sine");
+				ComboBox1.SelectedIndex = 0;
+
+				ComboBox2.Items.Add("EaseIn");
+				ComboBox2.Items.Add("EaseOut");
+				ComboBox2.Items.Add("EaseInOut");
+				ComboBox2.SelectedIndex = 0;
+			}
+			catch { }
+		}
+
+		private void Btn_Clicked(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				#region 原版
+				sBoard = new Storyboard();
+				DoubleAnimationUsingKeyFrames ks = new DoubleAnimationUsingKeyFrames();
+
+				EasingDoubleKeyFrame k1 = new EasingDoubleKeyFrame();
+				k1.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(0));
+				k1.Value = 0;
+
+				EasingMode easeMode2 = EasingMode.EaseIn;
+				if(ComboBox2.Text == "EaseOut")
+					easeMode2 = EasingMode.EaseOut;
+				else if (ComboBox2.Text == "EaseInOut")
+					easeMode2 = EasingMode.EaseInOut;
+
+				EasingDoubleKeyFrame k2 = new EasingDoubleKeyFrame();
+				k2.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromSeconds(Convert.ToInt32(TimeTextBox.Text)));
+				k2.Value = 400;
+				switch (ComboBox1.Text)
+				{
+					case "Back":
+						k2.EasingFunction = new BackEase() { EasingMode = easeMode2 };
+						break;
+					case "Bounce":
+						k2.EasingFunction = new BounceEase() { EasingMode = easeMode2 };
+						break;
+					case "Circle":
+						k2.EasingFunction = new CircleEase() { EasingMode = easeMode2 };
+						break;
+					case "Quadratic":
+						k2.EasingFunction = new QuadraticEase() { EasingMode = easeMode2 };
+						break;
+					case "Cubic":
+						k2.EasingFunction = new CubicEase() { EasingMode = easeMode2 };
+						break;
+					case "Quartic":
+						k2.EasingFunction = new QuarticEase() { EasingMode = easeMode2 };
+						break;
+					case "Quintic":
+						k2.EasingFunction = new QuinticEase() { EasingMode = easeMode2 };
+						break;
+					case "Elastic":
+						k2.EasingFunction = new ElasticEase() { EasingMode = easeMode2 };
+						break;
+					case "Exponential":
+						k2.EasingFunction = new ExponentialEase() { EasingMode = easeMode2 };
+						break;
+					case "Sine":
+						k2.EasingFunction = new SineEase() { EasingMode = easeMode2 };
+						break;
+
+				}
+
+				ks.KeyFrames.Add(k1);
+				ks.KeyFrames.Add(k2);
+				sBoard.Children.Add(ks);
+				Storyboard.SetTarget(ks, Img1);
+				Storyboard.SetTargetProperty(ks, new PropertyPath("(Canvas.Left)"));
+
+				sBoard.Completed += sBoard_Completed;
+				sBoard.Begin(Img1, true);
+				#endregion
+
+				#region 公式
+				easeMode = LEaseMode.EaseIn;
+				if (ComboBox2.Text == "EaseOut")
+					easeMode = LEaseMode.EaseOut;
+				else if (ComboBox2.Text == "EaseInOut")
+					easeMode = LEaseMode.EaseInOut;
+				
+				iDuration = Convert.ToInt32(Convert.ToInt32(TimeTextBox.Text) * 1000);
+				sType = ComboBox1.Text;
+				var v = Img2.GetValue(Canvas.LeftProperty);
+				new Thread(EaseAnimateHandle).Start();
+				#endregion
+
+			}
+			catch { }
+		}
+
+		private void sBoard_Completed(object sender, EventArgs e)
+		{
+			try
+			{
+				Thread.Sleep(1000);
+				sBoard.Remove(Img1);
+				Dispatcher.BeginInvoke(new Action<Image, double>(LeftHandle), Img1, 0d);
+			}
+			catch { }
+		}
+
+		private void LeftHandle(Image img, double d)
+		{
+			try
+			{
+				img.SetValue(Canvas.LeftProperty, d);
+			}
+			catch { }
+		}
+
+		private void EaseAnimateHandle()
+		{
+			try
+			{
+				int iInterval = 10;
+
+				for (int i = 0; i <= iDuration; i += iInterval)
+				{
+					double d0 = 0;
+					switch (sType)
+					{
+						case "Back":
+							d0 = AnimateBase.Back(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Bounce":
+							d0 = AnimateBase.Bounce(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Circle":
+							d0 = AnimateBase.Circle(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Quadratic":
+							d0 = AnimateBase.Quadratic(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Cubic":
+							d0 = AnimateBase.Cubic(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Quartic":
+							d0 = AnimateBase.Quartic(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Quintic":
+							d0 = AnimateBase.Quintic(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Elastic":
+							d0 = AnimateBase.Elastic(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Exponential":
+							d0 = AnimateBase.Exponential(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+						case "Sine":
+							d0 = AnimateBase.Sine(Convert.ToDouble(i) / iDuration, easeMode);
+							break;
+
+					}
+
+					double m = d0 * 400;
+
+					Dispatcher.BeginInvoke(new Action<Image, double>(LeftHandle), Img2, m);
+
+					Thread.Sleep(iInterval);
+				}
+
+				Thread.Sleep(1000);
+				Dispatcher.BeginInvoke(new Action<Image, double>(LeftHandle), Img2, 0d);
+			}
+			catch { }
+		}
+	}
+}
---
+++ other/EasingFunction/EasingFunction/Model/AnimateBase.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace EasingFunction
+{
+    public class AnimateBase
+    {
+        public static double Back(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return 2.70158 * x * x * x - 1.70158 * x * x;
+                case LEaseMode.EaseOut:
+                    return 1 + 2.70158 * Math.Pow(x - 1, 3) + 1.70158 * Math.Pow(x - 1, 2);
+                case LEaseMode.EaseInOut:
+                    return x < 0.5 ? (Math.Pow(2 * x, 2) * (7.189819 * x - 2.5949095)) / 2 : (Math.Pow(2 * x - 2, 2) * (7.189819 * x - 4.5949095) + 2) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private static double BounceOut(double x)
+        {
+            if (x < 1d / 2.75d) return 7.5625 * x * x;
+            else if (x < 2d / 2.75d) return 7.5625 * (x -= 1.5d / 2.75d) * x + 0.75;
+            else if(x<2.5d/2.75d) return 7.5625 * (x -= 2.25d / 2.75d) * x + 0.9375;
+            else return 7.5625 * (x -= 2.625d / 2.75d) * x + 0.984375;
+        }
+
+        public static double Bounce(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return 1 - BounceOut(1 - x);
+                case LEaseMode.EaseOut:
+                    return BounceOut(x);
+                case LEaseMode.EaseInOut:
+                    return x < 0.5 ? (1 - BounceOut(1 - 2 * x)) / 2 : (1 + BounceOut(2 * x - 1)) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Circle(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return 1 - Math.Sqrt(1 - Math.Pow(x, 2));
+                case LEaseMode.EaseOut:
+                    return Math.Sqrt(1 - Math.Pow(x - 1, 2));
+                case LEaseMode.EaseInOut:
+                    return x < 0.5 ? (1 - Math.Sqrt(1 - Math.Pow(2 * x, 2))) / 2 : (Math.Sqrt(1 - Math.Pow(-2 * x + 2, 2)) + 1) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Power(double x,LEaseMode easeMode,int power)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return Math.Pow(x, power);
+                case LEaseMode.EaseOut:
+                    return 1-Math.Pow(1-x,power);
+                case LEaseMode.EaseInOut:
+                    return x < 0.5 ? Math.Pow(2,power-1)*Math.Pow(x,power) : 1 - Math.Pow(-2 * x + 2, power) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Quadratic(double x, LEaseMode easeMode)
+        {
+            return Power(x, easeMode, 2);
+        }
+
+        public static double Cubic(double x, LEaseMode easeMode)
+        {
+            return Power(x, easeMode, 3);
+        }
+
+        public static double Quartic(double x, LEaseMode easeMode)
+        {
+            return Power(x, easeMode,4);
+        }
+
+        public static double Quintic(double x, LEaseMode easeMode)
+        {
+            return Power(x, easeMode, 5);
+        }
+
+        public static double Elastic(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return x == 0 ? 0 : x == 1 ? 1 : -Math.Pow(2, 10 * x - 10) * Math.Sin((x * 10 - 10.75) * 2 * Math.PI / 3);
+                case LEaseMode.EaseOut:
+                    return x == 0 ? 0 : x == 1 ? 1 : Math.Pow(2, -10 * x) * Math.Sin((x * 10 - 0.75) * 2 * Math.PI / 3)+1;
+                case LEaseMode.EaseInOut:
+                    return x == 0 ? 0 : x == 1 ? 1 : x < 0.5 ? -(Math.Pow(2, 20 * x - 10) * Math.Sin((x * 20 - 11.125) * 2 * Math.PI / 4.5)) / 2 : (Math.Pow(2, -20 * x + 10) * Math.Sin((x * 20 - 11.125) * 2 * Math.PI / 4.5)) / 2 + 1;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Exponential(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return x == 0 ? 0 : Math.Pow(2, 10 * x - 10); 
+                case LEaseMode.EaseOut:
+                    return x == 1 ? 1 : 1 - Math.Pow(2, -10 * x);
+                case LEaseMode.EaseInOut:
+                    return x == 0 ? 0 : x == 1 ? 1 : x < 0.5 ? Math.Pow(2, 20 * x - 10) / 2 : (2 - Math.Pow(2, -20 * x + 10)) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Sine(double x, LEaseMode easeMode)
+        {
+            switch (easeMode)
+            {
+                case LEaseMode.EaseIn:
+                    return 1 - Math.Cos((x * Math.PI) / 2);
+                case LEaseMode.EaseOut:
+                    return Math.Sin((x * Math.PI) / 2);
+                case LEaseMode.EaseInOut:
+                    return -(Math.Cos(Math.PI * x) - 1) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+    }
+}
