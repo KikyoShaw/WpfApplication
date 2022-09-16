35c18aa71ef983fc834d4552a04a79fa785ad132
shaw <1939484341@qq.com>
2022-09-16 17:14:52 +0800

    图片属性测试

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //该值确定是否可以选择多个文件,当前不然多选
+            dialog.Multiselect = false;
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "所有文件(*.*)|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var path = dialog.FileName;
+                var image = GetImageFromFile(path);
+
+                System.Drawing.Image img = System.Drawing.Image.FromFile(path);
+                float dpiX = img.HorizontalResolution;
+                float dpiY = img.VerticalResolution;
+            }
+        }
+
+        private BitmapImage GetImageFromFile(string sFilePath)
+        {
+            BitmapImage bmp = null;
+            if (File.Exists(sFilePath))
+            {
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(sFilePath);
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.EndInit();
+                bi.Freeze();
+                bmp = bi;
+            }
+            return bmp;
+        }
+    }
+}
