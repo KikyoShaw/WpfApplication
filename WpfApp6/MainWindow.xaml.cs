ee48bc079a7d7f164762ded535c1c2b8aa1e768c
shaw <1939484341@qq.com>
2023-05-25 16:14:23 +0800

    自适应缩进测试用例

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //TestDictionary();
+            //var slider = new Slider
+            //{
+            //    Width = 150,
+            //    Height = 30,
+            //    Opacity = 0.75,
+
+            //    VerticalAlignment = VerticalAlignment.Center,
+            //    Margin = new Thickness(0)
+            //};
+
+            //var adornerLayer = AdornerLayer.GetAdornerLayer(mainGrid);
+            //adornerLayer?.Add(new SliderAdorner(mainGrid, slider));
+        }
+
+        private void TestDictionary()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                // 添加项到字典中
+                { "item1", 1 },
+                { "sitem3", 7 },
+                { "item5", 3 },
+                { "item4", 4 }
+            };
+
+            foreach (var item in dict)
+            {
+               dict.Remove(item.Key);
+            }
+
+            List<int> list = new List<int>
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list.Remove(i);
+            }
+
+            foreach (var item in list)
+            {
+                if (item % 2 == 0)
+                {
+                    list.Remove(item);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp6/SliderAdorner.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfApp6
+{
+    public class SliderAdorner : Adorner
+    {
+        private readonly Slider _slider;
+
+        public SliderAdorner(UIElement adornedElement, Slider slider)
+            : base(adornedElement)
+        {
+            _slider = slider;
+            AddVisualChild(_slider);
+            AddLogicalChild(_slider);
+        }
+
+        protected override int VisualChildrenCount => 1;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _slider;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            _slider.Measure(constraint);
+            return _slider.DesiredSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _slider.Arrange(new Rect(finalSize));
+            return _slider.RenderSize;
+        }
+    }
+}
