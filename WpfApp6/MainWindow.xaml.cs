62cb3de7fbe38d6dfb9c3adb5689e731812793a8
shaw <1939484341@qq.com>
2023-07-13 15:58:09 +0800

    检测设备的CPU或网络负载

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 
 namespace WpfApp6
 {
@@ -26,6 +27,8 @@ namespace WpfApp6
 
             //var adornerLayer = AdornerLayer.GetAdornerLayer(mainGrid);
             //adornerLayer?.Add(new SliderAdorner(mainGrid, slider));
+
+            CheckPc();
         }
 
         private void TestDictionary()
@@ -41,12 +44,12 @@ namespace WpfApp6
 
             foreach (var item in dict)
             {
-               dict.Remove(item.Key);
+                dict.Remove(item.Key);
             }
 
             List<int> list = new List<int>
             {
-                1,2,3,4,5,6,7,8,9,10
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
             };
 
             for (int i = 0; i < list.Count; i++)
@@ -59,6 +62,49 @@ namespace WpfApp6
                 if (item % 2 == 0)
                 {
                     list.Remove(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 检测设备的CPU或网络负载
+        /// </summary>
+        private void CheckPc()
+        {
+            // 获取当前进程的CPU使用率
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            float cpuUsage = cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            cpuUsage = cpuCounter.NextValue();
+            System.Diagnostics.Trace.WriteLine($"CPU使用率: {cpuUsage}");
+
+            //网络
+            //var networkCounter = new PerformanceCounter("Network Interface", "Bytes Received/sec", "Network Interface Name");
+            //float receivedBytesPerSec = networkCounter.NextValue();
+            //System.Diagnostics.Trace.WriteLine($"receivedBytesPerSec:{receivedBytesPerSec}");
+            // 获取当前网络带宽使用率
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPv4InterfaceStatistics statistics =
+                        ni.GetIPv4Statistics();
+
+                    long bytesSentSpeed = 0;
+                    if (ni.Speed > 0)
+                    {
+                        bytesSentSpeed = (long)(statistics.BytesSent * 8) / ni.Speed * 100;
+                    }
+
+                    long bytesReceivedSpeed = 0;
+                    if (ni.Speed > 0)
+                    {
+                        bytesReceivedSpeed = (long)(statistics.BytesReceived * 8) / ni.Speed * 100;
+                    }
+
+                    System.Diagnostics.Trace.WriteLine($"网络接口名称：{ni.Name}");
+                    System.Diagnostics.Trace.WriteLine($"已发送数据速率：{bytesSentSpeed}");
+                    System.Diagnostics.Trace.WriteLine($"已接收数据速率：{bytesReceivedSpeed}");
                 }
             }
         }
