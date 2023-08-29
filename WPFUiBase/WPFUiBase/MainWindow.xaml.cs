ed73ae351a3baf7afbe08303de7f796232280416
shaw <1939484341@qq.com>
2023-08-29 15:53:09 +0800

    自定义win11 wpf窗口

---
+++ WPFUiBase/WPFUiBase/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace WPFUiBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UIBase.Window.BaseWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                var custom = new CustomTitleHeader();
+                CustomTitleHeader!.Children.Add(custom);
+                AddIgnoreCaptionElement(custom.CutomButton);
+            };
+        }
+    }
+}
