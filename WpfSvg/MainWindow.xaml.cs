7bcdd814b445fa4fef29a713afcd611bca589e38
shaw <1939484341@qq.com>
2024-05-23 14:39:12 +0800

    富文本实现 - 按钮+文字组合，文字支持换行和显示...

---
+++ WpfSvg/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfSvg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
