65153bbd3bf0672b3d1cd4e5977e7e0924205b10
shaw <1939484341@qq.com>
2024-05-28 16:01:47 +0800

    Clock 动画帧图播放组件实现

---
+++ Animation/AnimationFrameControl.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace Animation
+{
+    public class AnimationFrameControl : Image
+    {
+        private class FrameInfo
+        {
+            public BitmapImage? Bmp { get; set; }
+
+            public TimeSpan Duration { get; set; }
+        }
+
+        private ClockController? _clockController;
+        private AnimationClock? _clock;
+        //每帧的时间
+        private TimeSpan _frameTime = TimeSpan.FromMilliseconds(50);
+        //动画播放次数
+        RepeatBehavior _repeatBehavior = RepeatBehavior.Forever;
+
+        public AnimationFrameControl()
+        {
+        }
+
+        /// <summary>
+        /// 设置每帧的时间 - 默认50ms一帧
+        /// </summary>
+        public void SetFrameTime(TimeSpan frameTime)
+        {
+            _frameTime = frameTime;
+        }
+
+        /// <summary>
+        /// 设置动画播放次数 - 默认无限循环
+        /// </summary>
+        public void SetRepeatBehavior(int count)
+        {
+            _repeatBehavior = new RepeatBehavior(count);
+        }
+
+        /// <summary>
+        /// 启动动画
+        /// </summary>
+        public void StartAnimation()
+        {
+            _clockController?.Begin();
+        }
+
+        /// <summary>
+        /// 停止动画
+        /// </summary>
+        public void StopAnimation()
+        {
+            _clockController?.Stop();
+        }
+
+        /// <summary>
+        /// 设置动画的图片
+        /// </summary>
+        public async void InitAnimationAsync(string sPath)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(sPath))
+                    return;
+
+                var animationSources = await Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        var imageList = GetBitmapListFromPath(sPath);
+
+                        if (imageList == null)
+                            return null;
+
+                        var ret = new List<FrameInfo>();
+                        foreach (var bmp in imageList)
+                        {
+                            ret.Add(new FrameInfo()
+                            {
+                                Bmp = bmp,
+                                Duration = _frameTime
+                            });
+                        }
+                        return ret;
+                    }
+                    catch /*(Exception e)*/
+                    {
+                        //Console.WriteLine(e);
+                        //throw;
+                    }
+                    return null;
+                });
+
+                if (animationSources == null)
+                    return;
+
+                var keyFrames = new ObjectKeyFrameCollection();
+                var totalDuration = TimeSpan.Zero;
+                foreach (var image in animationSources)
+                {
+                    keyFrames.Add(new DiscreteObjectKeyFrame(image.Bmp, totalDuration));
+                    totalDuration += image.Duration;
+                }
+
+                var objectAnimation = new ObjectAnimationUsingKeyFrames()
+                {
+                    KeyFrames = keyFrames,
+                    Duration = totalDuration,
+                    RepeatBehavior = _repeatBehavior
+                };
+
+                _clock = objectAnimation.CreateClock();
+                _clockController = _clock.Controller;
+                _clock.Completed += (sender, args) => { _clockController?.Stop(); };
+                this.ApplyAnimationClock(SourceProperty, _clock);
+            }
+            catch /*(Exception e)*/
+            {
+                //Console.WriteLine(e);
+                //throw;
+            }
+        }
+
+        private static List<BitmapImage>? GetBitmapListFromPath(string sPath)
+        {
+            try
+            {
+                if (!Directory.Exists(sPath))
+                    return null;
+
+                DirectoryInfo cacheDir = new DirectoryInfo(sPath);
+                FileInfo[] cacheFiles = cacheDir.GetFiles();
+                if (cacheFiles.Length == 0)
+                {
+                    Directory.Delete(sPath, true);
+                    return null;
+                }
+
+                List<BitmapImage> vResultList = new List<BitmapImage>();
+                foreach (var f in cacheFiles)
+                {
+                    if (vResultList.Count >= 35)
+                        break;
+
+                    string fileType = f.Extension.ToLower();
+                    if (fileType != ".png" && fileType != ".bmp" && fileType != ".jpg" &&
+                        fileType != ".jpeg" && fileType != ".jpe")
+                    {
+                        continue;
+                    }
+
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.CacheOption = BitmapCacheOption.OnLoad;
+                    bmp.UriSource = new Uri(f.FullName);
+                    bmp.EndInit();
+                    bmp.Freeze();
+                    vResultList.Add(bmp);
+                }
+
+                return vResultList;
+            }
+            catch /*(Exception e)*/
+            {
+                //Console.WriteLine(e);
+                //throw;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ Animation/UserControl2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Animation
+{
+    /// <summary>
+    /// UserControl2.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControl2 : UserControl
+    {
+        public UserControl2()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    System.Windows.MessageBox.Show("文件夹路径不能为空", "提示");
+                    return;
+                }
+                var path = dialog.SelectedPath;
+                //AnimationControl.SetRepeatBehavior(1);
+                AnimationControl.InitAnimationAsync(path);
+            }
+        }
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            AnimationControl.StartAnimation();
+        }
+
+        private void ButtonBase3_OnClick(object sender, RoutedEventArgs e)
+        {
+            AnimationControl.StopAnimation();
+        }
+    }
+}
