91224b32e1f2b311206e99c6d026f79c74ecf1f7
shaw <1939484341@qq.com>
2024-05-31 17:16:21 +0800

    动画

--- WpfSvg/UserControl2.xaml.cs
+++ WpfSvg/UserControl2.xaml.cs
@@ -15,14 +15,22 @@ namespace WpfSvg
         public UserControl2()
         {
             InitializeComponent();
+            Init();
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            Init();
+            //_clockControllerA?.Begin();
+            //_clockControllerB?.Begin();
+
+            storyboard.Begin();
         }
 
         private Storyboard storyboard = new Storyboard();
+
+        private ClockController _clockControllerA;
+
+        private ClockController _clockControllerB;
 
         private void Init()
         {
@@ -34,8 +42,8 @@ namespace WpfSvg
             var animA = new DoubleAnimationUsingKeyFrames();
             animA.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(0.6)));
             animA.KeyFrames.Add(new LinearDoubleKeyFrame(-24, TimeSpan.FromSeconds(1.2)));
-            animA.KeyFrames.Add(new LinearDoubleKeyFrame(-24, TimeSpan.FromSeconds(3.2)));
-            animA.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(3.8)));
+            animA.KeyFrames.Add(new LinearDoubleKeyFrame(-24, TimeSpan.FromSeconds(11.2)));
+            animA.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(11.8)));
 
             //var animA = new DoubleAnimation(0, -54, TimeSpan.FromMilliseconds(600)) { BeginTime = TimeSpan.FromSeconds(2) };
             //transformA.BeginAnimation(TranslateTransform.YProperty, animA);
@@ -49,8 +57,23 @@ namespace WpfSvg
             var animB = new DoubleAnimationUsingKeyFrames();
             animB.KeyFrames.Add(new LinearDoubleKeyFrame(24, TimeSpan.FromSeconds(0.6)));
             animB.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(1.2)));
-            animB.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(3.2)));
-            animB.KeyFrames.Add(new LinearDoubleKeyFrame(24, TimeSpan.FromSeconds(3.8)));
+            animB.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(11.2)));
+            animB.KeyFrames.Add(new LinearDoubleKeyFrame(24, TimeSpan.FromSeconds(11.8)));
+
+            //创建并应用动画A的Clock
+            //animA.Freeze();
+            //AnimationClock clockA = animA.CreateClock();
+            //A.RenderTransform = new TranslateTransform() { X = 0, Y = 0 };
+            //A.RenderTransform.ApplyAnimationClock(TranslateTransform.YProperty, clockA);
+            //_clockControllerA = clockA.Controller;
+
+            //创建并应用动画B的Clock
+            //animB.Freeze();
+            //AnimationClock clockB = animB.CreateClock();
+            //B.RenderTransform = new TranslateTransform() { X = 0, Y = 24 };
+            //B.RenderTransform.ApplyAnimationClock(TranslateTransform.YProperty, clockB);
+            //_clockControllerB = clockB.Controller;
+
             //transformB.BeginAnimation(TranslateTransform.YProperty, animB);
 
             Storyboard.SetTarget(animA, A);
@@ -61,12 +84,19 @@ namespace WpfSvg
             Storyboard.SetTargetProperty(animB, new PropertyPath("RenderTransform.(TranslateTransform.Y)"));
             storyboard.Children.Add(animB);
 
-            storyboard.Begin();
         }
 
         private void ClearAnimation()
         {
             
+        }
+
+        private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
+        {
+            //_clockControllerA?.Stop();
+            //_clockControllerB?.Stop();
+
+            storyboard.Stop();
         }
     }
 }
